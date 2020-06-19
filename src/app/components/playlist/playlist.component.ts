46dc75a486edc06bc19e8918f0b613e866d999f6
Rien Croonenborghs <rien@croonenborghs.net>
2020-06-20 09:05:38 +1200

    playlist and viewer

---
+++ src/app/components/playlist/playlist.component.ts
@@ -0,0 +1,35 @@
+import { Component, OnInit } from '@angular/core';
+import { FormControl } from '@angular/forms';
+
+import { PlaylistService } from "@services/playlist.service";
+
+@Component({
+  selector: 'app-playlist',
+  templateUrl: './playlist.component.html',
+  styleUrls: ['./playlist.component.sass']
+})
+export class PlaylistComponent implements OnInit {
+
+  playlistURLControl = new FormControl();
+  streamURL: string = null;
+
+  constructor(
+    public playlistService: PlaylistService
+  ) { }
+
+  ngOnInit(): void {
+    this.playlistURLControl.setValue("http://i.mjh.nz/nzau/kodi-tv.m3u8");
+  }
+
+  loadPlaylist() {
+    this.playlistService.loadURL(this.playlistURLControl.value);
+  }
+
+  playStream(streamURL: string) {
+    this.streamURL = streamURL.split("|")[0];
+  }
+
+  stopPlaying() {
+    this.streamURL = null;
+  }
+}
