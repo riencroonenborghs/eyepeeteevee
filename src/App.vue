3112f4932aca5895749425b6f6f073061119477d
Rien Croonenborghs <rien@croonenborghs.net>
2020-07-30 19:47:34 +1200

    rearranging

--- src/App.vue
+++ src/App.vue
@@ -3,28 +3,28 @@
     <Toolbar></Toolbar>
 
     <md-tabs class="md-transparent">
-      <md-tab id="tab-url" md-icon="link">
-        <LoadURL v-on:loaded="onLoadChannels($event)"></LoadURL>
-      </md-tab>
-      <md-tab id="tab-file" md-icon="attach_file">
-        <LoadFile v-on:loaded="onLoadChannels($event)"></LoadFile>
-      </md-tab>
       <md-tab id="tab-countries" md-icon="flag">
         <LoadCountries v-on:loaded="onLoadChannels($event)"></LoadCountries>
       </md-tab>
       <md-tab id="tab-languages" md-icon="language">
         <LoadLanguages v-on:loaded="onLoadChannels($event)"></LoadLanguages>
       </md-tab>
+      <md-tab id="tab-url" md-icon="link">
+        <LoadURL v-on:loaded="onLoadChannels($event)"></LoadURL>
+      </md-tab>
+      <md-tab id="tab-file" md-icon="attach_file">
+        <LoadFile v-on:loaded="onLoadChannels($event)"></LoadFile>
+      </md-tab>
+      <md-tab id="tab-thanks" md-icon="thumb_up">
+        Thanks to
+        <a href="https://github.com/iptv-org/iptv" target="_blank">Github IPTV</a> and
+        <a href="https://www.matthuisman.nz" target="_blank">Matt Huisman</a>
+      </md-tab>
     </md-tabs>
 
     <flex-col>
       <Channels v-if="channels.length > 0" v-bind:channels="channels" v-on:play="onPlay($event)"></Channels>
     </flex-col>
-
-    <flex-row align-h="center" id="iptv-channels">
-      <a href="https://github.com/iptv-org/iptv" target="_blank">Github IPTV</a>
-      <a href="https://iptvcat.com/" target="_blank">IPTV Cat</a>
-    </flex-row>
 
     <md-dialog v-if="channel != null" :md-active.sync="showDialog" :md-close-on-esc="false" :md-click-outside-to-close="false">
       <md-dialog-title>
@@ -88,8 +88,8 @@ export default {
 </script>
 
 <style scoped>
-  #iptv-channels a {
+  #tab-thanks a {
     text-decoration: none;
-    padding: 4px;
+    padding-left: 4px;
   }
 </style>
